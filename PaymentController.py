bfbb19d2f1a81dfa07e6c70f55a9e5e3c72cafdf
Charles Ng <foonlife@gmail.com>
2013-07-09 13:19:46 +0800

    Second Commit

--- PaymentController.py
+++ PaymentController.py
@@ -11,37 +11,30 @@ import cgi
 
 PP_URL = "https://www.paypal.com/cgi-bin/webscr"
 PP_SANDBOX_URL = "https://www.paypal.com/webscr?cmd=_express-checkout&token="
-ACCOUNT_EMAIL = "foonlife@gmail.com"
-SANDBOX_ACCOUNT_EMAIL = "foonlife-facilitator@gmail.com"
+ACCOUNT_EMAIL = "xxxxxxxx@gmail.com"
+SANDBOX_ACCOUNT_EMAIL = "xxxxxxxx-facilitator@gmail.com"
 
 API_URL = 'https://api-3t.paypal.com/nvp'
 SANDBOX_API_URL = "https://api-3t.sandbox.paypal.com/nvp"
 ADD_NVP_PARAMS = { 
 
     # 3 Token Credentials
-    'USER' : 'foonlife_api1.gmail.com',
-    'PWD' : 'GXH3F6ZT7R7SEKGP',
-    'SIGNATURE' : 'ADwCenzdh3svFyQtpwkS0Qhn4sSZA3wUDEiitineHLX4KXyCa9-E4Wx3',
+    'USER' : 'xxxxxxxx_api1.gmail.com',
+    'PWD' : 'xxxxxxxx7R7SEKGP',
+    'SIGNATURE' : 'xxxxxxxxsvFyQtpwkS0Qhn4sSZA3wUDxxxxxxxxyCa9-E4Wx3',
     # API Version
     'VERSION' : '82.0'
 }   
 
 SANDBOX_ADD_NVP_PARAMS = {
 	# 3 Token Credentials
-    'USER' : 'foonlife-facilitator_api1.gmail.com',
-    'PWD' : '1365343946',
-    'SIGNATURE' : 'AQU0e5vuZCvSg-XJploSa.sGUDlpAcJLj99Nn6CgDExvyN5-iAS3J4wy',
+    'USER' : 'xxxxxxxx-facilitator_api1.gmail.com',
+    'PWD' : 'xxxxx43946',
+    'SIGNATURE' : 'xxxxxxxx-XJploSa.sGUDlpAcJLj99Nn6CgDExvyN5-iAS3J4wy',
     # API Version
     'VERSION' : '82.0'
 }
-adaptive_headers = {
-    'X-PAYPAL-SECURITY-USERID' : 'XXX',
-    'X-PAYPAL-SECURITY-PASSWORD' : 'XXX',
-    'X-PAYPAL-SECURITY-SIGNATURE' : 'XXX',
-    'X-PAYPAL-REQUEST-DATA-FORMAT' : 'JSON',
-    'X-PAYPAL-RESPONSE-DATA-FORMAT' : 'JSON',
-    'X-PAYPAL-APPLICATION-ID' : 'APP-80W284485P519543T' # Sandbox
-}
+
 
 seller_email = 'foonlife@gmail.com'
 
@@ -65,10 +58,7 @@ class ProceedToPaymentHandler(Handler.Handler):
 
 class PurchaseCreditsHandler(Handler.Handler):
 	def get(self):
-		#for debugging successful page
-		#task_id = "5402067753030909952"
-		#self.render("successful-purchase.html", task_id=task_id)
-		#return
+		
 		username = self.request.cookies.get("username").split("|")[0]
 		a_User = Models.User.all().filter("username =", username).get()
 		params = {}
@@ -312,158 +302,3 @@ class ExpressCheckout(object):
 			return "https://www.paypal.com/incontext?token=%s" % (token,)
 		
 
-
-"""
-class PaymentHandler(Handler.Handler):
-	def post(self, mode=""):
-		if mode == "set_ec":
-			sid = self.request.cookies.get("username").split("|")[1]
-			
-			quantity = self.request.get("quantity")
-			if len(quantity) == 0:
-				self.render("cancel-purchase.html")
-				return
-				
-				
-			self.set_quantity_cookie(quantity)
-			product = "credits"
-			price = 0.2
-			amount = price * int(quantity)
-			#logging.info("amount %s" % amount)
-			nvp_params = {
-				'L_PAYMENTREQUEST_0_NAME0' : str(quantity) + ' ' + product,
-				'L_PAYMENTREQUEST_0_QTY0' : quantity,
-				'L_PAYMENTREQUEST_0_AMT0' : str(price),
-				'L_PAYMENTREQUEST_0_ITEMCATEGORY0' : 'Digital',
-				'PAYMENTREQUEST_0_CURRENCYCODE': 'HKD',
-				'PAYMENTREQUEST_0_ITEMAMT' : str(amount),
-				'PAYMENTREQUEST_0_AMT' : str(amount),
-				'RETURNURL' : self.request.host_url+"/do_ec_payment?sid="+sid,
-				'CANCELURL': self.request.host_url+"/cancel_ec?sid="+sid
-			}
-			#self.write(nvp_params)
-			#return
-			response = ExpressCheckout.set_express_checkout(nvp_params)
-			
-			if response.status_code != 200:
-				logging.info("Failure for SetExpress Checkout")
-				
-				template_values = {
-					'title' : 'Error',
-					'operation' : 'SetExpressCheckout'
-				}
-				
-				return self.write(template_values)
-				
-				#return self.render_form_login_required("unknown-error.html", template_values=template_values)
-				
-			# The remainder of the transaction is completed in context
-			
-			parsed_qs = cgi.parse_qs(response.content)
-			
-			#logging.info(parsed_qs)
-			#self.write(parsed_qs)
-			#return
-			redirect_url = ExpressCheckout.generate_express_checkout_digital_goods_redirect_url(parsed_qs['TOKEN'][0])
-			
-			return self.redirect(redirect_url)
-				
-		else:
-			logging.error("Unknown mode for POST request!")
-	def get(self, mode=""):
-		if mode == "do_ec_payment":
-			sid = self.request.cookies.get('sid')
-			payerid = self.request.get("PayerID")
-			quantity = self.request.cookies.get('qp')
-			logging.info("quantity %s" % quantity)
-			product = "credits"
-			price = 0.2
-			amount = int(quantity) * price
-			
-			nvp_params = { 
-				'PAYERID' : payerid, 
-				'PAYMENTREQUEST_0_CURRENCYCODE': 'HKD',
-				'PAYMENTREQUEST_0_AMT' : str(amount)
-			}
-			
-			response = ExpressCheckout.do_express_checkout_payment(
-						self.request.get("token"), nvp_params)
-			#logging.info("response %s" % response)
-			if response.status_code != 200:
-				#logging.error("Failure for DoExpressCheckoutPayment")
-				template_values = {
-					'title' : 'Error',
-					'operation' : 'DoExpressCheckoutPayment'
-				}
-				logging.info("Unsuccessful Operations")
-				return self.write("Unsuccessful Operations")
-			
-			parsed_qs = cgi.parse_qs(response.content)
-			logging.info(parsed_qs)
-			if parsed_qs['ACK'][0] != 'Success':
-				logging.error("Unsuccessful DoExpressCheckoutPayment")
-				
-				template_values = {
-					'title' : 'Error',
-					'details' : parsed_qs['L_LONGMESSAGE0'][0]
-				}
-				
-			template_values = {
-				'title' : 'Successful Payment',
-			}
-			
-			#credit payment
-			username = self.request.cookies.get("username").split('|')[0]
-			a_User = Models.User.get_user_by_name(username)
-			quantity = int(quantity)
-			a_CP = Models.CreditsPurchase(sid=sid,user=a_User,quantity=quantity)
-			a_CP.put()
-			if quantity == 300:
-				quantity = quantity + 100
-			elif quantity == 500:
-				quantity = quantity + 200
-			elif quantity == 1000:
-				quantity = quantity + 300
-			
-			a_User.credits += quantity
-			a_User.experience += 15
-			
-			a_User.put()
-			utils.single_user_cache(a_User.key().id(), update=True)
-			utils.single_user_cache_by_name(a_User.username, update=True)
-			self.render("/cancel-purchase.html")
-			self.write("payment succeeded!")
-			
-		elif mode == "get_ec_details":
-			response = ExpressCheckout.get_express_checkout_details(self.request.get("token"))
-			#self.write(response.status_code)
-			#return
-			if response.status_code != 200:
-				logging.error("Failure for GetExpressCheckoutDetails")
-				template_values = {
-					'title' : 'Error',
-					'operation' : 'GetExpressCheckoutDetails'
-				}
-				
-				self.write("Error")
-				
-			product = "credits"
-			parsed_qs = cgi.parse_qs(response.content)
-			
-			template_values = {
-				'title' : 'Confirm Purchase',
-				'quantity' : 1,
-				#'email' : parsed_qs['EMAIL'][0],
-				'amount' : parsed_qs['PAYMENTREQUEST_0_AMT'][0],
-				'query_string_params' : self.request.query_string 
-			}
-			#self.render_form_login_required("confirm-purchase.html", template_values=template_values)
-				
-		elif mode == "cancel_ec":
-			template_values = {
-				'title': 'Cancel Purchase'
-			}
-			self.render("/cancel-purchase.html")
-
-
-"""
